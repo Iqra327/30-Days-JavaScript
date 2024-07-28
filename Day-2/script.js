5f3c5f5302896a77673f85250408e77157b31422
Iqra327 <iqrarasheed219@gmail.com>
2024-07-28 21:29:58 +0500

    operations and operators

---
+++ Day-2/script.js
@@ -0,0 +1,74 @@
+//Task1
+let num1 = 4;
+let num2 = 5;
+console.log(num1 + num2);
+
+//Task2
+ num1 = 5;
+ num2 = 4;
+ console.log(num1 - num2);
+
+ //Task3
+ num1 = 4;
+ num2 = 6;
+ console.log(num1 * num2);
+
+ //Task4
+ num1 = 8;
+ num2 = 4;
+ console.log(num1 / num2);
+
+ //Task5
+ num1 = 4;
+ num2 = 2;
+ console.log(num1 % num2);
+
+ //Task6
+ num1 = 10;
+ num2 = 4;
+ console.log(num1 += num2);
+
+//Task7
+num1 = 10;
+num2 = 6;
+console.log(num1 -= num2);
+
+//Task8
+num1 = 6;
+num2 = 4;
+console.log(num1 > num2);
+
+//Task9
+num1 = 8;
+num2 = 5;
+console.log(num1 <= num2);
+
+//Task10
+num1 = 9;
+num2 = 9;
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+//Task11
+num1 = 2;
+num2 = 4;
+console.log(num1 < num2 && num1 > num2);
+
+//Task12
+num1 = 7;
+num2 = 9;
+console.log(num1 < num2 || num1 > num2);
+
+//Task13
+num1 = 3;
+num2 = 4;
+console.log(num1 != num2);
+
+//Task14
+let value = 4;
+let ans = value < 0 ? 'Value is negative' : 'Value is positive'
+console.log(ans);
+
+value = -6;
+ans = value > 0 ? 'Value is positive' : 'Value is negative';
+console.log(ans);
