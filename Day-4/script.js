0e1140c466c63186d2151b46f0cb94483bc95914
Iqra327 <iqrarasheed219@gmail.com>
2024-07-31 22:49:17 +0500

    initial-commit

---
+++ Day-4/script.js
@@ -0,0 +1,38 @@
+//Task1
+for(let i =1; i <= 10; i++){
+  console.log(i);
+}
+
+//Task2
+for(let i = 1 ; i <=10; i++){
+  console.log(i * 5);
+}
+
+//Task3
+let i = 1;
+while(i <= 10){
+  console.log(i + i);
+  i++;
+}
+
+//Task4
+ let n = 10;
+ while(n >= 1){
+  console.log(n);
+  n--;
+ }
+
+ //Task5
+ let x = 1;
+ do{
+  console.log(x);
+  x++;
+ }
+ while(x<=5);
+
+ //Task6
+for(let i = 1; i<=5; i++){
+  for(let y = 1; y<=5 ; y+=2){
+    console.log('*' * '*');
+  }
+}
