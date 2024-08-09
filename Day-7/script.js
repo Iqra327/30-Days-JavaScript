b2a784dad622e3bfebe8a89501c0845e5af37e65
Iqra327 <iqrarasheed219@gmail.com>
2024-08-09 23:10:03 +0500

    in process

---
+++ Day-7/script.js
@@ -0,0 +1,13 @@
+//Task1 object creation and access
+let book = {
+  title: 'Islamic Book',
+  author: 'Some person',
+  year: '2020'
+};
+console.log(book);
+
+//Task2
+console.log(`Title of book is: ${book.title}`);
+console.log(`Author of book is: ${book.author}`);
+
+//Task3
