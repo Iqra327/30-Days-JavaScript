ca6ddb57be515e0fb322648dc51cd57396ed0935
Iqra327 <iqrarasheed219@gmail.com>
2024-08-10 21:32:00 +0500

    in process

--- Day-7/script.js
+++ Day-7/script.js
@@ -10,4 +10,37 @@ console.log(book);
 console.log(`Title of book is: ${book.title}`);
 console.log(`Author of book is: ${book.author}`);
 
-//Task3
+//Task3 Object methods
+book = {
+  author: 'some person',
+  title: 'Islamic Book',
+  authorTitleName: function(){
+    return `${this.title} ${this.author}`;
+  }
+}
+
+console.log(book.authorTitleName());
+
+//Task4
+book = {
+  year: '2020',
+  updateYear: function(){
+    return this.year = 2022;
+  }
+}
+  console.log(book.updateYear());
+
+//Task5 Nested Objects
+const library = {
+  name: 'Aena Library',
+  books: [
+    {'book1Name' : 'book1'},
+    {'book2Name' : 'book2'}
+  ]
+}
+
+console.log(library);
+
+//Task6
+
+//Task7
