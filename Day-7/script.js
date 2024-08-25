ef8b63c223e2df5c7b93ae3cba842aa26a79b592
Iqra327 <iqrarasheed219@gmail.com>
2024-08-25 23:52:52 +0500

    all about objects, object iteration, method, this keyword, object.keys/values/entries etc

--- Day-7/script.js
+++ Day-7/script.js
@@ -28,19 +28,67 @@ book = {
     return this.year = 2022;
   }
 }
-  console.log(book.updateYear());
+book.updateYear();
+console.log(book.year);
 
 //Task5 Nested Objects
 const library = {
   name: 'Aena Library',
   books: [
-    {'book1Name' : 'book1'},
-    {'book2Name' : 'book2'}
+    {'bookName' : 'book1'},
+    {'bookName' : 'book2'}
   ]
 }
 
 console.log(library);
 
 //Task6
+library.books.map( book => console.log(book.bookName));
 
-//Task7
+//Task7 this keyword
+const library2 = {
+  name: 'Aena Library',
+  book : [
+    {
+    bookTitle: 'Nature',
+    Year: 2024
+    }
+  ],
+  display: function(){
+    return (`Book title: ${this.book[0].bookTitle} , Year: ${this.book[0].Year}`);
+  }
+};
+
+console.log(library2.display());
+
+//Task 8 Object Iteration
+const lib = {
+  name: 'Aena Library',
+  Year: 2022,
+  Title: 'Do fun' 
+};
+
+for(const x in lib){
+  console.log(`${x}: ${lib[x]}`)
+}
+
+//Task9
+
+//Object.keys return the property names
+const properties = Object.keys(lib);
+console.log(properties);
+
+//Object.values returns the property Values
+const values = Object.values(lib);
+console.log(values);
+
+//just applying for each loop on values
+values.forEach((name) => {
+  let propertyValue = '';
+  propertyValue += name;
+  console.log(propertyValue);
+});
+
+//Object.entries return the whole object
+const object = Object.entries(lib);
+console.log(object);
